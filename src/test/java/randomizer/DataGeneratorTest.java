44a20ffb7598e4133a26035f5f45b80d8d238b89
urszula.mazur <urszula.mazur@mail.com>
2019-07-12 13:50:44 +0200

    Tests cd

--- src/test/java/randomizer/DataGeneratorTest.java
+++ src/test/java/randomizer/DataGeneratorTest.java
@@ -8,118 +8,97 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.time.LocalDate;
 import java.util.stream.Stream;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 class DataGeneratorTest {
 
 
-    static Stream<Pair<LocalDate, LocalDate>> setUp() {
+    static Stream<Pair<LocalDate, LocalDate>> setUpDatesExpired() {
         DataGenerator dr = new DataGenerator();
         return Stream.of(dr.getExpiredPeriod(), dr.getExpiredPeriod(), dr.getExpiredPeriod(), dr.getExpiredPeriod(),
                 dr.getExpiredPeriod(), dr.getExpiredPeriod(), dr.getExpiredPeriod(), dr.getExpiredPeriod());
     }
 
     @ParameterizedTest
-    @MethodSource("setUp")
+    @MethodSource("setUpDatesExpired")
     void getExpiredPeriod(Pair<LocalDate, LocalDate> date) {
         assertTrue(date.fst.isBefore(date.snd));
         assertTrue(date.snd.isBefore(LocalDate.now()));
     }
 
-    private void assertTrue(boolean before) {
+    static Stream<Pair<LocalDate, LocalDate>> setUpDatesNotStarted() {
+        DataGenerator dr = new DataGenerator();
+        return Stream.of(dr.getNotStartedPeriod(), dr.getNotStartedPeriod(), dr.getNotStartedPeriod(), dr.getNotStartedPeriod(),
+                dr.getNotStartedPeriod(), dr.getNotStartedPeriod(), dr.getNotStartedPeriod(), dr.getNotStartedPeriod());
     }
 
-    @Test
-    void getNotStartedPeriod() {
-        DataGenerator dr = new DataGenerator();
-        Pair<LocalDate, LocalDate> date = dr.getNotStartedPeriod();
-        System.out.println(date.fst);
-        System.out.println(date.snd);
-        Pair<LocalDate, LocalDate> date1 = dr.getNotStartedPeriod();
-        System.out.println(date1.fst);
-        System.out.println(date1.snd);
-        Pair<LocalDate, LocalDate> date2 = dr.getNotStartedPeriod();
-        System.out.println(date2.fst);
-        System.out.println(date2.snd);
-        Pair<LocalDate, LocalDate> date3 = dr.getNotStartedPeriod();
-        System.out.println(date3.fst);
-        System.out.println(date3.snd);
-        Pair<LocalDate, LocalDate> date4 = dr.getNotStartedPeriod();
-        System.out.println(date4.fst);
-        System.out.println(date4.snd);
+    @ParameterizedTest
+    @MethodSource("setUpDatesNotStarted")
+    void getNotStartedPeriod(Pair<LocalDate, LocalDate> date) {
+        boolean isNull = (date == null);
+        if (!isNull) {
+            assertTrue(date.fst.isBefore(date.snd));
+            assertTrue(date.fst.isAfter(LocalDate.now()));
+        }
     }
 
-    @Test
-    void getPeriod() {
+    static Stream<Pair<LocalDate, LocalDate>> setUpDates() {
         DataGenerator dr = new DataGenerator();
-        Pair<LocalDate, LocalDate> date = dr.getPeriod();
-        System.out.println(date.fst);
-        System.out.println(date.snd);
-        Pair<LocalDate, LocalDate> date1 = dr.getPeriod();
-        System.out.println(date1.fst);
-        System.out.println(date1.snd);
-        Pair<LocalDate, LocalDate> date2 = dr.getPeriod();
-        System.out.println(date2.fst);
-        System.out.println(date2.snd);
-        Pair<LocalDate, LocalDate> date3 = dr.getPeriod();
-        System.out.println(date3.fst);
-        System.out.println(date3.snd);
-        Pair<LocalDate, LocalDate> date4 = dr.getPeriod();
-        System.out.println(date4.fst);
-        System.out.println(date4.snd);
+        return Stream.of(dr.getPeriod(), dr.getPeriod(), dr.getPeriod(), dr.getPeriod(),
+                dr.getPeriod(), dr.getPeriod(), dr.getPeriod(), dr.getPeriod());
     }
 
-    @Test
-    void getValidPeriod() {
-        DataGenerator dr = new DataGenerator();
-        Pair<LocalDate, LocalDate> date = dr.getValidPeriod();
-        System.out.println(date.fst);
-        System.out.println(date.snd);
-        Pair<LocalDate, LocalDate> date1 = dr.getValidPeriod();
-        System.out.println(date1.fst);
-        System.out.println(date1.snd);
-        Pair<LocalDate, LocalDate> date2 = dr.getValidPeriod();
-        System.out.println(date2.fst);
-        System.out.println(date2.snd);
-        Pair<LocalDate, LocalDate> date3 = dr.getValidPeriod();
-        System.out.println(date3.fst);
-        System.out.println(date3.snd);
-        Pair<LocalDate, LocalDate> date4 = dr.getValidPeriod();
-        System.out.println(date4.fst);
-        System.out.println(date4.snd);
+    @ParameterizedTest
+    @MethodSource("setUpDates")
+    void getPeriod(Pair<LocalDate, LocalDate> date) {
+        boolean isNull = (date == null);
+        if (!isNull) {
+            assertTrue(date.fst.isBefore(date.snd));
+        }
     }
 
-    @Test
-    void getValidYesterdayPeriod() {
+    static Stream<Pair<LocalDate, LocalDate>> setUpDatesValid() {
         DataGenerator dr = new DataGenerator();
-        Pair<LocalDate, LocalDate> date = dr.getValidYesterdayPeriod();
-        System.out.println(date.fst);
-        System.out.println(date.snd);
-        Pair<LocalDate, LocalDate> date1 = dr.getValidYesterdayPeriod();
-        System.out.println(date1.fst);
-        System.out.println(date1.snd);
-        Pair<LocalDate, LocalDate> date2 = dr.getValidYesterdayPeriod();
-        System.out.println(date2.fst);
-        System.out.println(date2.snd);
-        Pair<LocalDate, LocalDate> date3 = dr.getValidYesterdayPeriod();
-        System.out.println(date3.fst);
-        System.out.println(date3.snd);
-        Pair<LocalDate, LocalDate> date4 = dr.getValidYesterdayPeriod();
-        System.out.println(date4.fst);
-        System.out.println(date4.snd);
+        return Stream.of(dr.getValidPeriod(), dr.getValidPeriod(), dr.getValidPeriod(), dr.getValidPeriod(),
+                dr.getValidPeriod(), dr.getValidPeriod(), dr.getValidPeriod(), dr.getValidPeriod());
+    }
+
+    @ParameterizedTest
+    @MethodSource("setUpDatesValid")
+    void getValidPeriod(Pair<LocalDate, LocalDate> date) {
+        assertTrue(date.fst.isBefore(date.snd));
+        assertTrue(date.fst.isBefore(LocalDate.now()));
+        assertTrue(date.snd.isAfter(LocalDate.now()));
+    }
+
+    static Stream<Pair<LocalDate, LocalDate>> setUpDatesValidYesterday() {
+        DataGenerator dr = new DataGenerator();
+        return Stream.of(dr.getValidYesterdayPeriod(), dr.getValidYesterdayPeriod(), dr.getValidYesterdayPeriod(), dr.getValidYesterdayPeriod(),
+                dr.getValidYesterdayPeriod(), dr.getValidYesterdayPeriod(), dr.getValidYesterdayPeriod(), dr.getValidYesterdayPeriod());
+    }
+
+    @ParameterizedTest
+    @MethodSource("setUpDatesValidYesterday")
+    void getValidYesterdayPeriod(Pair<LocalDate, LocalDate> date) {
+        assertTrue(date.fst.isBefore(date.snd));
+        assertTrue(date.fst.isBefore(LocalDate.now()));
+        assertEquals(date.snd, LocalDate.now().minusDays(1));
     }
 
     @Test
     void getValidTodayDate() {
         DataGenerator dr = new DataGenerator();
         LocalDate date = dr.getValidTodayDate();
-        System.out.println(date);
+        assertEquals(date, LocalDate.now());
     }
 
     @Test
     void getValidYesterdayDate() {
         DataGenerator dr = new DataGenerator();
         LocalDate date = dr.getValidYesterdayDate();
-        System.out.println(date);
+        assertEquals(date, LocalDate.now().minusDays(1));
     }
 
     @Test
@@ -157,12 +136,40 @@ class DataGeneratorTest {
         System.out.println(dr.getActivationDate(LocalDate.now()).toString());
     }
 
-    @Test
-    void getName() {
+    static Stream<String> setUpNames() {
+        DataGenerator dr = new DataGenerator();
+        return Stream.of(dr.getName(), dr.getName(), dr.getName(), dr.getName(),
+                dr.getName(), dr.getName(), dr.getName(), dr.getName());
     }
 
-    @Test
-    void getSurname() {
+    @ParameterizedTest
+    @MethodSource("setUpNames")
+    void getName(String name) {
+        assertTrue(name.length() > 1);
+    }
+
+    static Stream<String> setUpSurnames() {
+        DataGenerator dr = new DataGenerator();
+        return Stream.of(dr.getSurname(), dr.getSurname(), dr.getSurname(), dr.getSurname(),
+                dr.getSurname(), dr.getSurname(), dr.getSurname(), dr.getSurname());
+    }
+
+    @ParameterizedTest
+    @MethodSource("setUpSurnames")
+    void getSurname(String name) {
+        assertTrue(name.length() > 1);
+    }
+
+    static Stream<String> setUpPasses() {
+        DataGenerator dr = new DataGenerator();
+        return Stream.of(dr.getPassportNumber(), dr.getPassportNumber(), dr.getPassportNumber(), dr.getPassportNumber(),
+                dr.getPassportNumber(), dr.getPassportNumber(), dr.getPassportNumber(), dr.getPassportNumber());
+    }
+
+    @ParameterizedTest
+    @MethodSource("setUpPasses")
+    void getPassportNumber(String pass) {
+        assertTrue(pass.length() > 7);
     }
 
     @Test
