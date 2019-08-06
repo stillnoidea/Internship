4ded1074ed98ec9172369671d249681f18e464cb
urszula.mazur <urszula.mazur@mail.com>
2019-08-06 14:13:47 +0200

    String Generator junit test

--- src/main/java/com/example/demo/StringGenerator.java
+++ src/main/java/com/example/demo/StringGenerator.java
@@ -1,28 +1,35 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.annotation.Testable;
+
 import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class StringGenerator {
 
     private String text;
     private final static String ALPHA_NUMERIC_STRING = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
             + "0123456789";
+    private final static int MAX_TEXT_LENGTH = 180;
 
     public StringGenerator() {
-        generateText();
+        text = generateText();
     }
 
-    private void generateText() {
+    private String generateText() {
         int size = generateTextLength();
         StringBuilder sb = new StringBuilder(size);
         for (int i = 0; i < size; i++) {
             sb.append(getRandomChar());
         }
-        text = sb.toString();
+        return sb.toString();
     }
 
     private int generateTextLength() {
-        return new Random().nextInt();
+        return new Random().nextInt(MAX_TEXT_LENGTH);
     }
 
     private char getRandomChar() {
@@ -32,5 +39,30 @@ public class StringGenerator {
 
     public String getText() {
         return text;
+    }
+
+    @Testable
+    class StringGeneratorTest {
+
+        @Test
+        void getRandomCharTest() {
+            char chr = getRandomChar();
+            int index = ALPHA_NUMERIC_STRING.indexOf(chr);
+            assertTrue(index >= 0);
+        }
+
+        @Test
+        void generateTextLengthTest() {
+            int number = generateTextLength();
+            assertTrue(number > 0);
+            assertTrue(number < 180);
+        }
+
+        @Test
+        void generateTextTest() {
+            String text = generateText();
+            assertTrue(text.length() < MAX_TEXT_LENGTH);
+            assertFalse(text.isEmpty());
+        }
     }
 }
---
+++ src/test/java/com/example/demo/StringGeneratorTest.java
@@ -0,0 +1,5 @@
+package com.example.demo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
