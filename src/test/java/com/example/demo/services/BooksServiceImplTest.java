cdbe5c08c8e4af4471dccb5bb1a7d31af6f5974b
urszula.mazur <urszula.mazur@mail.com>
2019-08-08 13:57:44 +0200

    Test implementation

---
+++ src/test/java/com/example/demo/services/BooksServiceImplTest.java
@@ -0,0 +1,43 @@
+package com.example.demo.services;
+
+import com.example.demo.repository.MockedBookRepositoryImpl;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BooksServiceImplTest {
+    @Autowired
+    private MockedBookRepositoryImpl mockedBookRepository;
+    @Autowired
+    private BooksServiceImpl service;
+
+    @Test
+    public void shouldContainsCurlyBrackets() {
+        String booksInfo = service.getBooksInfo(mockedBookRepository);
+        assertTrue(booksInfo.contains("{"));
+        assertTrue(booksInfo.contains("}"));
+    }
+
+    @Test
+    public void shouldContainsColon() {
+        String booksInfo = service.getBooksInfo(mockedBookRepository);
+        assertTrue(booksInfo.contains(":"));
+    }
+
+    @Test
+    public void shouldContainsDefaultWords() {
+        String booksInfo = service.getBooksInfo(mockedBookRepository);
+        assertTrue(booksInfo.contains("title"));
+        assertTrue(booksInfo.contains("author"));
+        assertTrue(booksInfo.contains("releaseDate"));
+        assertTrue(booksInfo.contains("numberOfPages"));
+        assertTrue(booksInfo.contains("language"));
+    }
+
+}
