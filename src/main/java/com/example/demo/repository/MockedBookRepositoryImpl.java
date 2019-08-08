8e3519c240360316d4e255ed18e1ddb54a581161
urszula.mazur <urszula.mazur@mail.com>
2019-08-08 13:32:23 +0200

    Manual initialization of a few books

--- src/main/java/com/example/demo/repository/MockedBookRepositoryImpl.java
+++ src/main/java/com/example/demo/repository/MockedBookRepositoryImpl.java
@@ -1,8 +1,10 @@
 package com.example.demo.repository;
 
 import com.example.demo.model.Book;
+import com.example.demo.model.util.Language;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,6 +15,7 @@ public class MockedBookRepositoryImpl implements BookRepository {
 
     MockedBookRepositoryImpl() {
         this.books = new ArrayList<>(15);
+        initializeBooks();
     }
 
     @Override
@@ -20,4 +23,11 @@ public class MockedBookRepositoryImpl implements BookRepository {
         return books;
     }
 
+    private void initializeBooks() {
+        books.add(new Book("Harry Potter and the Philosopher's Stone", "J.K. Rowling", LocalDate.of(1997, 6, 26), 223, Language.ENGLISH));
+        books.add(new Book("Harry Potter and the Philosopher's Stone", "J.K. Rowling", LocalDate.of(1997, 6, 26), 223, Language.ENGLISH));
+        books.add(new Book("Harry Potter and the Philosopher's Stone", "J.K. Rowling", LocalDate.of(1997, 6, 26), 223, Language.ENGLISH));
+        books.add(new Book("Harry Potter and the Philosopher's Stone", "J.K. Rowling", LocalDate.of(1997, 6, 26), 223, Language.ENGLISH));
+        books.add(new Book("Harry Potter and the Philosopher's Stone", "J.K. Rowling", LocalDate.of(1997, 6, 26), 223, Language.ENGLISH));
+    }
 }
