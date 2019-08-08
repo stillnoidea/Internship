6057e280f46392783ac64da07721c4a0eb3093d9
urszula.mazur <urszula.mazur@mail.com>
2019-08-08 13:47:44 +0200

    Optimized import

--- src/test/java/com/example/demo/controller/StringControllerTest.java
+++ src/test/java/com/example/demo/controller/StringControllerTest.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
