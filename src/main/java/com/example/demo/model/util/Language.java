6e14d9cfd0551330b58fd423d69fba7e124d0256
urszula.mazur <urszula.mazur@mail.com>
2019-08-08 10:50:55 +0200

    Added enum with languages to use as part of the data in Book class

---
+++ src/main/java/com/example/demo/model/util/Language.java
@@ -0,0 +1,29 @@
+package com.example.demo.model.util;
+
+public enum Language {
+    ENGLISH("English"),
+    POLISH("Polish"),
+    SWEDISH("Swedish"),
+    AMERICAN("American"),
+    BELGIAN("Belgian"),
+    DUTCH("Dutch"),
+    GERMAN("German"),
+    GREEK("Greek"),
+    FRENCH("French"),
+    FINNISH("Finnish"),
+    ITALIAN("Italian"),
+    RUSSIAN("Russian"),
+    SPANISH("Spanish"),
+    TURKISH("Turkish");
+
+    String name;
+
+    Language(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
