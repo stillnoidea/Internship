9b1a14e252b4f59210866884e52e5f46f9923e3c
urszula.mazur <urszula.mazur@mail.com>
2019-08-08 13:33:34 +0200

    Changed toString method so returned string is in json format

--- src/main/java/com/example/demo/model/Book.java
+++ src/main/java/com/example/demo/model/Book.java
@@ -1,45 +1,66 @@
 package com.example.demo.model;
 
 import com.example.demo.model.util.Language;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import java.time.LocalDate;
 
 public class Book {
+
     private String title;
     private String author;
     private LocalDate releaseDate;
     private int numberOfPages;
     private Language language;
 
-    public String getMainInfo() {
-        StringBuilder mainInfo = new StringBuilder();
-
-        mainInfo.append("Book: ")
-                .append(title)
-                .append("\n")
-                .append("Author: ")
-                .append(author);
-
-        return mainInfo.toString();
-    }
-
-    public String getDetails() {
-        StringBuilder details = new StringBuilder();
-
-        details.append("Release date: ")
-                .append(releaseDate)
-                .append("\n")
-                .append("Number of pages: ")
-                .append(numberOfPages)
-                .append("\n")
-                .append("Language: ")
-                .append(language.toString());
-
-        return details.toString();
+    public Book(String title, String author, LocalDate releaseDate, int numberOfPages, Language language) {
+        this.title = title;
+        this.author = author;
+        this.releaseDate = releaseDate;
+        this.numberOfPages = numberOfPages;
+        this.language = language;
     }
 
     @Override
     public String toString() {
-        return getMainInfo() + "\n" + getDetails();
+        String result = "";
+
+        try {
+            result = writeJson();
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    private String writeJson() throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        return objectMapper.registerModule(new JavaTimeModule())
+                .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+                .writeValueAsString(this);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public int getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    public Language getLanguage() {
+        return language;
     }
 }
