6f834a76bb2263ae7ebbaf94f4a69ba93e51176b
urszula.mazur <urszula.mazur@mail.com>
2019-08-08 12:00:03 +0200

    Implementation of repository, that holds books in a List, for MockedBookRepository it's arrayList

--- src/main/java/com/example/demo/repository/BookRepository.java
+++ src/main/java/com/example/demo/repository/BookRepository.java
@@ -1,5 +1,7 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 public interface BookRepository {
-    void findAll();
+    List findAll();
 }
--- src/main/java/com/example/demo/repository/MockedBookRepositoryImpl.java
+++ src/main/java/com/example/demo/repository/MockedBookRepositoryImpl.java
@@ -1,9 +1,23 @@
 package com.example.demo.repository;
 
-public class MockedBookRepositoryImpl implements BookRepository
-{
-    @Override
-    public void findAll() {
+import com.example.demo.model.Book;
+import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class MockedBookRepositoryImpl implements BookRepository {
+
+    private ArrayList<Book> books;
+
+    MockedBookRepositoryImpl(ArrayList<Book> books) {
+        this.books = books;
     }
+
+    @Override
+    public List findAll() {
+        return books;
+    }
+
 }
