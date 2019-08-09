94c4b3574216440ec19d16ae2307a63a51b1f74e
urszula.mazur <urszula.mazur@mail.com>
2019-08-09 10:54:56 +0200

    changed test for book despite it's changes

--- src/test/java/com/example/demo/model/BookTest.java
+++ src/test/java/com/example/demo/model/BookTest.java
@@ -1,42 +1,15 @@
 package com.example.demo.model;
 
-import com.example.demo.model.util.Language;
-import org.junit.Before;
 import org.junit.Test;
 
-import java.time.LocalDate;
-
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 public class BookTest {
 
-    private Book book;
-    private String bookInfo;
-
-    @Before
-    public void setUp() {
-        book = new Book("Harry Potter and the Philosopher's Stone", "J.K. Rowling",
-                LocalDate.of(1997, 6, 26), 223, Language.ENGLISH);
-        bookInfo = book.toString();
-    }
-
     @Test
-    public void shouldContainsDefaultWords() {
-        assertTrue(bookInfo.contains("title"));
-        assertTrue(bookInfo.contains("author"));
-        assertTrue(bookInfo.contains("releaseDate"));
-        assertTrue(bookInfo.contains("numberOfPages"));
-        assertTrue(bookInfo.contains("language"));
-    }
-
-    @Test
-    public void shouldContainCurlyBracket() {
-        assertTrue(bookInfo.contains("{"));
-        assertTrue(bookInfo.contains("}"));
-    }
-
-    @Test
-    public void shouldContainColon() {
-        assertTrue(bookInfo.contains(":"));
+    public void setAuthorTest() {
+        Book book = new Book();
+        book.setAuthor("James Williams");
+        assertEquals("James Williams", book.getAuthor());
     }
 }
