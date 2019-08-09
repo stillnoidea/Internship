d2521fcba2d56a2facb65ad36788335ebbb4f70a
urszula.mazur <urszula.mazur@mail.com>
2019-08-09 10:54:31 +0200

    Get rid of toString method and writeToJson method from book, added setters

--- src/main/java/com/example/demo/model/Book.java
+++ src/main/java/com/example/demo/model/Book.java
@@ -1,10 +1,6 @@
 package com.example.demo.model;
 
 import com.example.demo.model.util.Language;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import java.time.LocalDate;
 
@@ -16,32 +12,15 @@ public class Book {
     private int numberOfPages;
     private Language language;
 
+    public Book() {
+    }
+
     public Book(String title, String author, LocalDate releaseDate, int numberOfPages, Language language) {
         this.title = title;
         this.author = author;
         this.releaseDate = releaseDate;
         this.numberOfPages = numberOfPages;
         this.language = language;
-    }
-
-    @Override
-    public String toString() {
-        String result = "";
-
-        try {
-            result = writeJson();
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
-        return result;
-    }
-
-    private String writeJson() throws JsonProcessingException {
-        ObjectMapper objectMapper = new ObjectMapper();
-
-        return objectMapper.registerModule(new JavaTimeModule())
-                .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
-                .writeValueAsString(this);
     }
 
     public String getTitle() {
@@ -62,5 +41,25 @@ public class Book {
 
     public String getLanguage() {
         return language.toString();
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public void setNumberOfPages(int numberOfPages) {
+        this.numberOfPages = numberOfPages;
+    }
+
+    public void setLanguage(Language language) {
+        this.language = language;
     }
 }
