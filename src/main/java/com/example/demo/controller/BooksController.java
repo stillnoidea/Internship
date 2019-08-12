da47f46f5e7237c6dc46466e4960888c5c279000
urszula.mazur <urszula.mazur@mail.com>
2019-08-12 13:46:29 +0200

    Added endpoint used to filter books from library using author, title and/or language

--- src/main/java/com/example/demo/controller/BooksController.java
+++ src/main/java/com/example/demo/controller/BooksController.java
@@ -1,12 +1,14 @@
 package com.example.demo.controller;
 
 import com.example.demo.services.BooksServiceImpl;
+import com.example.demo.dto.BookMainInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.InvalidParameterException;
 import java.util.List;
 
 @RestController
@@ -29,5 +31,14 @@ public class BooksController {
     public @ResponseBody
     List search(@RequestParam(name = "word") String soughtWord) {
         return service.findBooksContainingWord(soughtWord);
+    }
+
+    @RequestMapping("/filter")
+    public @ResponseBody
+    List filter(BookMainInfo bookParams) {
+        List result = service.findBooksWithParams(bookParams);
+        if (result == null) {
+            throw new InvalidParameterException("There must be at least one parameter");
+        } else return result;
     }
 }
