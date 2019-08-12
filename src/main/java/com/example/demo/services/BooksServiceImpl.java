c73a0e0b33814a6867827ef75385d5c88d2354d4
urszula.mazur <urszula.mazur@mail.com>
2019-08-12 13:45:12 +0200

    Added new methods for filtering books by author, title and/or language

--- src/main/java/com/example/demo/services/BooksServiceImpl.java
+++ src/main/java/com/example/demo/services/BooksServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.demo.services;
 
 import com.example.demo.model.Book;
 import com.example.demo.repository.BookRepository;
+import com.example.demo.dto.BookMainInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -29,11 +30,51 @@ public class BooksServiceImpl implements BookService {
     }
 
     private Stream<Book> booksContainingWord(String word) {
-        Stream<Book> bookStream = findAll().stream();
+        Stream<Book> bookStream = getBooksStream();
         return bookStream.filter(book -> isBookContainingWord(book, word));
     }
 
+    private Stream<Book> getBooksStream() {
+        return findAll().stream();
+    }
+
     private boolean isBookContainingWord(Book book, String word) {
-        return book.getAuthor().contains(word)||book.getTitle().contains(word)||book.getLanguage().contains(word);
+        return isAuthorMatchingWord(book, word) || isTitleMatchingWord(book, word) || isLanguageMatchingWord(book, word);
+    }
+
+    private boolean isAuthorMatchingWord(Book book, String author) {
+        if (author != null) {
+            return book.getAuthor().contains(author);
+        } else return true;
+    }
+
+    private boolean isTitleMatchingWord(Book book, String title) {
+        if (title != null) {
+            return book.getTitle().contains(title);
+        } else return true;
+    }
+
+    private boolean isLanguageMatchingWord(Book book, String language) {
+        if (language != null) {
+            return book.getLanguage().contains(language);
+        } else return true;
+    }
+
+    public List<Book> findBooksWithParams(BookMainInfo bookParams) {
+        if (bookParams.getTitle() == null && bookParams.getAuthor() == null && bookParams.getLanguage() == null) {
+            return null;
+        }
+        return findBooksWithAllParams(bookParams);
+    }
+
+    private List<Book> findBooksWithAllParams(BookMainInfo bookParams) {
+        return booksWithParams(bookParams).collect(Collectors.toList());
+    }
+
+    private Stream<Book> booksWithParams(BookMainInfo bookParams) {
+        Stream<Book> bookStream = getBooksStream();
+        return bookStream.filter(book -> isTitleMatchingWord(book, bookParams.getTitle())
+                && isAuthorMatchingWord(book, bookParams.getAuthor())
+                && isLanguageMatchingWord(book, bookParams.getLanguage()));
     }
 }
