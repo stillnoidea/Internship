0afedbef9ddce0d078fdd6f7203c9cc6828eb4db
urszula.mazur <urszula.mazur@mail.com>
2019-08-13 09:50:41 +0200

    Changed field to use interface instead of interface implementation, get rid of ResponseBody annotation, because was useless

--- src/main/java/com/example/demo/controller/BooksController.java
+++ src/main/java/com/example/demo/controller/BooksController.java
@@ -1,11 +1,11 @@
 package com.example.demo.controller;
 
-import com.example.demo.services.BooksServiceImpl;
 import com.example.demo.dto.BookMainInfo;
+import com.example.demo.model.Book;
+import com.example.demo.services.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.InvalidParameterException;
@@ -14,29 +14,26 @@ import java.util.List;
 @RestController
 public class BooksController {
 
-    private final BooksServiceImpl service;
+    private final BookService service;
 
     @Autowired
-    public BooksController(BooksServiceImpl service) {
+    public BooksController(BookService service) {
         this.service = service;
     }
 
-    @RequestMapping("/library")
-    public @ResponseBody
-    List getLibrary() {
+    @GetMapping("/library")
+    public List<Book> getLibrary() {
         return service.findAll();
     }
 
-    @RequestMapping("/search")
-    public @ResponseBody
-    List search(@RequestParam(name = "word") String soughtWord) {
+    @GetMapping("/search")
+    public List<Book> search(@RequestParam(name = "word") String soughtWord) {
         return service.findBooksContainingWord(soughtWord);
     }
 
-    @RequestMapping("/filter")
-    public @ResponseBody
-    List filter(BookMainInfo bookParams) {
-        List result = service.findBooksWithParams(bookParams);
+    @GetMapping("/filter")
+    public List<Book> filter(BookMainInfo bookParams) {
+        List<Book> result = service.findBooksWithParams(bookParams);
         if (result == null) {
             throw new InvalidParameterException("There must be at least one parameter");
         } else return result;
