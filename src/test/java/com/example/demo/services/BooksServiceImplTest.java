6e526c4613cdff157ad7aa8ab61be35e9ecc3a82
urszula.mazur <urszula.mazur@mail.com>
2019-08-13 09:56:17 +0200

    test implementation to new method

--- src/test/java/com/example/demo/services/BooksServiceImplTest.java
+++ src/test/java/com/example/demo/services/BooksServiceImplTest.java
@@ -1,9 +1,9 @@
 package com.example.demo.services;
 
+import com.example.demo.dto.BookMainInfo;
 import com.example.demo.model.Book;
 import com.example.demo.model.util.Language;
 import com.example.demo.repository.BookRepository;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -16,16 +16,17 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.*;
 
 @RunWith(MockitoJUnitRunner.class)
 @SpringBootTest
 public class BooksServiceImplTest {
     @Mock
     private BookRepository bookRepository;
+    @Mock
+    private BookMainInfo bookMainInfo;
     @InjectMocks
-    private BooksServiceImpl service;
+    private BookServiceImpl service;
     private List<Book> books;
 
     @Test
@@ -61,5 +62,50 @@ public class BooksServiceImplTest {
         list.add(new Book("Harry Potter and the Philosopher's Stone", "J.K. Rowling", LocalDate.of(1997, 6, 26), 223, Language.ENGLISH));
         list.add(new Book("The Great Gatsby", "F. Scott Fitzgerald", LocalDate.of(1926, 2, 10), 218, Language.AMERICAN));
         return list;
+    }
+
+    @Test
+    public void shouldReturnNull() {
+        assertNull(service.findBooksWithParams(bookMainInfo));
+    }
+
+    @Test
+    public void shouldReturnEmptyList() {
+        Mockito.when(bookMainInfo.getAuthor()).thenReturn("Carl");
+        Mockito.when(service.findAll()).thenReturn(initializeBooksListMock());
+        assertTrue(service.findBooksWithParams(bookMainInfo).isEmpty());
+    }
+
+    @Test
+    public void shouldReturnOneElementList() {
+        Mockito.when(bookMainInfo.getAuthor()).thenReturn("Rowling");
+        Mockito.when(service.findAll()).thenReturn(initializeBooksListMock());
+        List<Book> books = service.findBooksWithParams(bookMainInfo);
+        assertEquals(1, books.size());
+        assertEquals("J.K. Rowling", books.get(0).getAuthor());
+        assertEquals(223, books.get(0).getNumberOfPages());
+    }
+
+    @Test
+    public void shouldReturnTwoElementList() {
+        Mockito.when(bookMainInfo.getTitle()).thenReturn("a");
+        Mockito.when(service.findAll()).thenReturn(initializeBooksListMock());
+        List<Book> books = service.findBooksWithParams(bookMainInfo);
+        assertEquals(2, books.size());
+        assertEquals("J.K. Rowling", books.get(0).getAuthor());
+        assertEquals(223, books.get(0).getNumberOfPages());
+        assertEquals("F. Scott Fitzgerald", books.get(1).getAuthor());
+        assertEquals(218, books.get(1).getNumberOfPages());
+    }
+
+    @Test
+    public void shouldReturnHarryPotter() {
+        Mockito.when(bookMainInfo.getTitle()).thenReturn("a");
+        Mockito.when(bookMainInfo.getAuthor()).thenReturn("Row");
+        Mockito.when(service.findAll()).thenReturn(initializeBooksListMock());
+        List<Book> books = service.findBooksWithParams(bookMainInfo);
+        assertEquals(1, books.size());
+        assertEquals("J.K. Rowling", books.get(0).getAuthor());
+        assertEquals(223, books.get(0).getNumberOfPages());
     }
 }
