8c853b6cc005aa502d27fa637c1d67384a8bf7ad
urszula.mazur <urszula.mazur@mail.com>
2019-08-14 11:24:50 +0200

    updated StringController due to changed methods in StringGeneratorService

--- src/main/java/com/example/demo/controller/StringController.java
+++ src/main/java/com/example/demo/controller/StringController.java
@@ -1,8 +1,6 @@
 package com.example.demo.controller;
 
 import com.example.demo.services.StringGeneratorService;
-import com.example.demo.services.StringGeneratorServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,15 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class StringController {
 
-    private final StringGeneratorService service;
-
-    @Autowired
-    public StringController(StringGeneratorServiceImpl service) {
-        this.service = service;
-    }
-
     @RequestMapping("/start")
-    public @ResponseBody String start() {
-        return service.getText();
+    public @ResponseBody
+    String start() {
+        return StringGeneratorService.getText();
     }
 }
